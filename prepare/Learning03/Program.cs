6913743d169691a0f8532e28819b5aa8a534a6b4
Jenna Ray <jennastingray@gmail.com>
2023-06-20 13:37:27 -0600

    03 prepare

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(2, 6);
+
+        f3.SetTop(2);
+        Console.WriteLine(f3.GetBottom());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
     }
 }
